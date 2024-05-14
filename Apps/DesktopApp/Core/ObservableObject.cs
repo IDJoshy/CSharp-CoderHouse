c7d6d7a17c4b2478b770ca516cf608549cceea8c
IDJoshy <112592102+IDJoshy@users.noreply.github.com>
2024-05-14 13:38:03 -0400

    Update

---
+++ Apps/DesktopApp/Core/ObservableObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace DesktopApp.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Apps/DesktopApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DesktopApp.Core;
+
+
+namespace DesktopApp.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel() 
+        {
+
+        }
+
+    }
+}
